37e6232708800cc6027d380ce84a7cb1fe555f52
niloufar_alinezhad <niloufar_alinezhad@fdeshragh.com>
2026-06-08 06:19:26 +0330

    first

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import './ui/global.css'
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
